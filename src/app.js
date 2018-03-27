98962ffd59ec95793eb637f6ee0d72758aaa74ee
Mateusz Miodek <mmiodek@gmail.com>
2018-03-27 22:55:40 +0200

    added modules

--- src/app.js
+++ src/app.js
@@ -1,27 +1,6 @@
-let Post = {
-  findAll(){
-    return new Promise((resolve,reject) => {
-      let uri = "http://localhost:3000/posts";
-      let request = new XMLHttpRequest();
-      request.open("GET",uri,true);
-      request.onload = () => {
-        if(request.status >=200 && request.status < 400){
-          resolve(JSON.parse(request.response));
-        }
-      };
-      request.onerror = () => {
-        reject(new Error("Something wrong with the API"));
-      }
+import Post from "./post";
+import ui from "./ui";
 
-      request.send();
-    });
-  }
-}
-
-let ui = {
-  renderPosts(posts){
-    console.log(posts);
-  }
-}
-
-Post.findAll().then(ui.renderPosts).catch((error) => {console.log(error)});
+Post.findAll()
+  .then(ui.renderPosts)
+  .catch((error) => {console.log(error)});
