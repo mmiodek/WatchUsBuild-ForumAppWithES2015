a8b9adc615fa4bae2d8f3b0a864529cf46617b96
Mateusz Miodek <mmiodek@gmail.com>
2018-03-27 23:16:53 +0200

    fix for declering modules

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,22 @@
 let ui = {
   renderPosts(posts){
-    console.log(posts);
+    let elements = posts.map((post) => {
+      return articleTemplate;
+    });
+
+    let target = document.querySelector(".container");
+    target.innerHTML = elements;
   }
 }
+
+let articleTemplate = `<article class='post'>
+    <h2 class='post-title'>
+      ${safeTitle}
+    </h2>
+    <p class='post-meta'>
+      last reply on ${safeLastReply}
+    </p>
+  </article>`;
+
 
 export default ui;
